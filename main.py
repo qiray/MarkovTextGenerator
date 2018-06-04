0de6d18b9dbda40d6fc68ade9dedd3eb87e407f2
qiray <zotovyaa@mail.ru>
2018-06-04 22:24:52 +0300

    scheme created

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+"""
+
+PREPARE:
+1) Read text
+2) Parse tokens
+3) Save to DB
+
+DB:
+
+pairs:
+start
+end
+probability
+type (maybe token is not a simple word/comma/dot/dash/etc or 2-3-4-word sequence)
+
+starts:
+token
+
+ends:
+token
+
+GENERATE:
+1) Init sequence with token from starts
+2) While token has ends:
+	- get ends for token
+	- select token from ends (according to probability)
+	- append token to sequence
+3) Return sequence
+
+"""
