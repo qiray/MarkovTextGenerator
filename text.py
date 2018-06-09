983771598305e7446214a0d19bf7ef33baf52fba
qiray <zotovyaa@mail.ru>
2018-06-09 14:39:56 +0300

    improved parse

--- text.py
+++ text.py
@@ -9,24 +9,23 @@ import database as db
 # 2) Parse tokens
 # 3) Save to DB
 
-ENDINGS_RE = "[!.?\n]"
-N = 1 #used for different sizes - trigrams, digrams etc
+N = 2 #used for different sizes - trigrams, digrams etc
 
 def read_text(path):
     """Read and parse text file"""
     with open(path, 'r', encoding='utf-8') as f: #read file
         read_data = f.read()
-        senteces = re.split(ENDINGS_RE, read_data) #split line by senteces
-        for sentece in senteces:
-            tokens = parse_tokens(sentece, N) #parse each sentece
+        sentences = re.findall(r'[^!.?\n]*[.?!]+', read_data) #split line by sentences
+        for sentence in sentences:
+            tokens = parse_tokens(sentence, N) #parse each sentence
             save_to_db(tokens)
 
 def parse_tokens(text, size):
-    """Parse sentece and return tokens"""
+    """Parse sentence and return tokens"""
     text = text.replace(r"\s+", " ") #replace multiple spaces
-    words = re.findall(r"[\w-]+|[^\w\s]", text, re.UNICODE) #parse sentece into tokens
+    words = re.findall(r"[\w-]+|[^\w\s]", text, re.UNICODE) #parse sentence into tokens
     # words = re.findall(r"\w+\s*|[^\w\s]\s*", text, re.UNICODE) #with spaces
-    if not words: #empty sentece
+    if not words: #empty sentence
         return {}
     length = len(words)
     lists = []
