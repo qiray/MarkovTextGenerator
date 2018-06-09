13ff24ed62b7f0394c04a01c062eb9cd17c56ac0
qiray <zotovyaa@mail.ru>
2018-06-09 16:34:24 +0300

    tokens generation added

--- tokens.py
+++ tokens.py
@@ -2,12 +2,11 @@
 
 class Token(object):
 
-    def __init__(self, begin, end, is_begin, is_end):
+    def __init__(self, begin, end, is_begin=0, is_end=0):
         self.begin = begin
         self.end = end
         self.is_begin = is_begin
         self.is_end = is_end
 
-    def func(self, param):
-        if param:
-            pass
+    def __str__(self):
+        return self.begin + ' ' + self.end + ' (%d %d)'% (self.is_begin, self.is_end)
