8cc5726c38376946df819547d8ccb60eda0c40c2
qiray <zotovyaa@mail.ru>
2018-06-19 21:46:28 +0300

    some database improvements + nltk usage

--- text.py
+++ text.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+'''This module is used for preparing database with text data'''
 
 import sys
 import re
-from functools import reduce
+from nltk import sent_tokenize #for text parsing
 import database as db
 import tokens
 
@@ -18,18 +19,19 @@ def read_text(path):
     """Read and parse text file"""
     with open(path, 'r', encoding='utf-8') as f: #read file
         read_data = f.read()
-        sentences = re.findall(r'[^!.?\n]*[.?!]+', read_data) #split line by sentences
+        #sentences = re.findall(r'[^!.?\n]*[.?!\n]+(?=[ \n])', read_data) #split line by sentences
+        sentences = sent_tokenize(read_data) #use nltk to parse text into sentences
         conn, cursor = db.start_connection()
         for sentence in sentences:
-            tokens = parse_tokens(sentence, N) #parse each sentence
-            db.save_tokens(tokens, cursor)
-        db.end_connecion(conn, cursor)
+            current_tokens = parse_tokens(sentence, N) #parse each sentence
+            db.save_tokens(current_tokens, cursor)
+        db.end_connecion(conn)
 
 def parse_tokens(text, size):
     """Parse sentence and return tokens"""
     text = text.replace(r"\s+", " ") #replace multiple spaces
 
-    string_start = STRING_START_TEXT * (N - 1) 
+    string_start = STRING_START_TEXT * (size - 1)
     text = string_start + text #append to text some special start tokens
 
     words = re.findall(r"[\w-]+|[^\w\s]", text, re.UNICODE) #parse sentence into tokens
@@ -47,10 +49,10 @@ def parse_tokens(text, size):
     result = []
     length = len(lists)
     for i in range(0, length):
-        is_begin = 1 if i <= N - 1 else 0
-        is_end = 1 if i + N >= length - 1 else 0
+        is_begin = 1 if i <= size - 1 else 0
+        is_end = 1 if i + size >= length - 1 else 0
         start = ' '.join(lists[i]) if i < length else ''
-        end = ' '.join(lists[i + N]) if i + N < length else ''
+        end = ' '.join(lists[i + size]) if i + size < length else ''
         token = tokens.Token(start, end, is_begin, is_end)
         result.append(token)
     return result
