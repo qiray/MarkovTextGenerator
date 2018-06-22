26c323cd8122f45f294ca1be747ebe6c004f7804
qiray <zotovyaa@mail.ru>
2018-06-22 18:06:55 +0300

    N usage restored

--- main.py
+++ main.py
@@ -11,46 +11,46 @@ VERSION_MAJOR = 0
 VERSION_MINOR = 0
 VERSION_BUILD = 1
 
-N = 3 #used for different sizes - trigrams, digrams etc
-
 def get_version():
     '''Get app version'''
     return "{}.{}.{}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD)
 
-def get_random_token(data, count):
+def get_random_pair(data, count):
     '''Get random token from pairs according to probability'''
     index = random.randint(0, count - 1)
     start_value, stop_value = 0, 0
     for obj in data:
         stop_value += obj[2]
         if start_value <= index and index < stop_value:
-            return obj[1]
+            return obj
         start_value += obj[2]
-    return ''
+    return ['', '', 0]
 
 def list_to_sentence(tokens_list):
     '''Convert list of tokens to sentence'''
     result = ' '.join(tokens_list)
+    result = result.replace(text.STRING_START_TEXT, '')
     result = re.sub(r'\s+', ' ', result).strip() #remove multiple spaces
     result = re.sub(r'\s([.,!?:)/])', r'\g<1>', result).strip()
     #TODO: check punctuation etc
     return result
 
-def get_next_token_for_start(start):
-    data, count = database.get_pairs_for_start(start)
-    return get_random_token(data, count)
+def get_next_pair(tokens_list, number):
+    #TODO: use number for different sizes - trigrams, digrams etc
+    data, count = database.get_pairs_for_start(tokens_list[-1])
+    return get_random_pair(data, count)
 
 def generate_sequence(number):
     """Function for generating sentences"""
-    #TODO: use number for different sizes - trigrams, digrams etc
     start = database.get_start_token()
     if not start:
         return ''
-    next_token = get_next_token_for_start(start) #get all pairs for selected start
-    tokens_list = [start, next_token]
-    while not database.is_token_end(next_token): #while chosen pair is not end
-        next_token = get_next_token_for_start(next_token) # get another list of pairs
-        tokens_list.append(next_token)
+    tokens_list = [start]
+    pair = get_next_pair(tokens_list, number) #get all pairs for selected start
+    tokens_list.append(pair[1])
+    while not database.is_pair_end(pair): #while chosen pair is not end
+        pair = get_next_pair(tokens_list, number) # get another list of pairs
+        tokens_list.append(pair[1])
     return list_to_sentence(tokens_list)
 
 def parse_args():
@@ -58,7 +58,7 @@ def parse_args():
     parser = argparse.ArgumentParser(prog='MarkovTextGenerator', description='Markov text generator v {}'.format(get_version()))
     parser.add_argument('--init', action='store_true', help='Init database only')
     parser.add_argument('-f', '--parse', help='Parse text file and save it in database')
-    parser.add_argument("-n", "--number", help='Set size of token for text parsing (default = {})'.format(N), type=int)
+    parser.add_argument("-n", "--number", help='Set size of token for text parsing (default = {})'.format(text.N), type=int)
     parser.add_argument("-g", "--generate", help='Generate text sequence', action='store_true')
     parser.add_argument("-v", "--version", help='Show version', action='store_true')
     return parser.parse_args()
@@ -66,20 +66,22 @@ def parse_args():
 def main():
     """Main function"""
     random.seed()
-    number = N
 
-    args = parse_args() #parse command line arguments
-    if args.init:
-        database.init_db()
-    if args.number:
-        number = args.number
-    if args.parse:
-        text.read_text(args.parse)
-    if args.generate:
-        sentence = generate_sequence(number)
-        print(sentence)
-    if args.version:
-        print("Markov text generator v {}".format(get_version()))
+    try:
+        args = parse_args() #parse command line arguments
+        if args.init:
+            database.init_db()
+        if args.number:
+            text.set_n_value(args.number)
+        if args.parse:
+            text.read_text(args.parse)
+        if args.generate:
+            sentence = generate_sequence(text.N)
+            print(sentence)
+        if args.version:
+            print("Markov text generator v {}".format(get_version()))
+    except FileNotFoundError:
+        print("Can't read data file {}".format(args.parse))
 
 #Now call main function
 main()
